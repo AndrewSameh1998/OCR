6702b10366eff43e15b8e8bc6d2e2fa3f4a52a32
Your Name <nayernabil141@hotmail.com>
2019-12-15 16:43:12 +0200

    Added Project File

---
+++ Final Project.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import sys
+
+img = cv2.imread('test_sample12.jpg', 0)  # 331, 328 Search for an image full of circles
+cv2.imshow('before', img)
+edges = cv2.Canny(img,127,255)
+cv2.imshow('after', edges)
+# Circles will be detected using Contours and hit&miss algorithm
+
+cv2.waitKey()
