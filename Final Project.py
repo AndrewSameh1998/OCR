e2d465d71286ab564b804d1f0eb67801bfebb9c8
Nano141 <nayernabil141@hotmail.com>
2019-12-22 21:49:24 +0200

    Calibrated the pixel values and produced final output

--- Final Project.py
+++ Final Project.py
@@ -1,86 +1,117 @@
 import cv2
 import numpy as np
 import math
-from scipy import ndimage
 
-img = cv2.imread('test_sample8.jpg', 0)# 331, 328 Search for an image full of circles
+
+def rotateImage(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+
+file = open("Output.txt","w+")
+img = cv2.imread('test_sample2.jpg', 0)
 img = cv2.resize(img,(481,680))
-cv2.imshow("Original_Img", img)
 img = cv2.bitwise_not(img)
-#img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 img_edges = cv2.Canny(img, 100, 100, apertureSize=3)
 lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=50, maxLineGap=5)
 angles = []
 for x1, y1, x2, y2 in lines[0]:
-    #cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
     angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
     angles.append(angle)
 
 median_angle = np.median(angles)
-img = ndimage.rotate(img, median_angle)
-#print("Angle is {}".format(median_angle))
-cv2.imshow('Rotated', img)
-# scale_percent = 30 # percent of original size
-# width = int(img.shape[1] * scale_percent / 100)
-# height = int(img.shape[0] * scale_percent / 100)
-# dim = (width, height)
-# img = cv2.resize(img, dim)
-# cv2.imshow('Resized', img)
-ret, thresh = cv2.threshold(img,235,255,cv2.THRESH_BINARY)
-cv2.imshow('threshed', thresh)
-#kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-# kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-# cv2.imshow('Thresh 2', thresh)
-# answers = cv2.connectedComponentsWithStats(thresh, 4, cv2.CV_32S)
-# answers = answers[2]
-# answers = answers[1:23]
-# print("Number Of Circles", len(answers))
-# print("Array", answers)
-#
-# def Answer (Question_no, x_axis,y_axis):
-#     if (i[0] >= 292) & (i[0] <= 302) & (i[1] >= 236):
-#         print('Question: %5.1f Strongly Agree'%(Question_no))
-#     if (i[0] >= 318) & (i[0] <= 328) & (i[1] >= 236):
-#         print('Question: %5.1f Agree' % (Question_no))
-#     if (i[0] >= 345) & (i[0] <= 355) & (i[1] >= 236):
-#         print('Question: %5.1f Neutral' % (Question_no))
-#     if (i[0] >= 371) & (i[0] <= 381) & (i[1] >= 236):
-#         print('Question: %5.1f Disagree' % (Question_no))
-#     if (i[0] >= 398) & (i[0] <= 408) & (i[1] >= 236):
-#         print('Question: %5.1f Strongly Disagree' % (Question_no))
-# value = 255
-# count = 1.1
-# count2 = 1.0
-# j = 0
-# while j != 19:
-#     for i in circles[0]:
-#         if (i[1] >= value) & (i[1] <= (value+11)):
-#             print(i[1])
-#             print(value)
-#             Answer(count,0,0)
-#             if (count == 4.2):
-#                 value = value - 20
-#             else:
-#                 value = value + 10
-#             count = round(count + 0.1,1)
-#             j += 1
-#             print(i[1])
-#             print (value)
-#             if ((round(count-count2,1)) == 0.6) & (count2 == 1.0):
-#                 count = 2.1
-#                 count2 += 1.0
-#                 value += 25
-#             if (round(count-count2,1) == 0.7) & (count2 == 2.0):
-#                 count = 3.1
-#                 count2 += 1.0
-#                 value += 25
-#             if (round(count-count2,1) == 0.4) & (count2 >= 3.0):
-#                 if count2 == 3.0:
-#                     count = 4.1
-#                     count2 += 1.0
-#                     value += 25
-#                 else:
-#                     count = 5.1
 
+img = rotateImage(img,median_angle)
+img = cv2.resize(img,(481,680))
+
+ret, thresh = cv2.threshold(img,235,255,cv2.THRESH_BINARY)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+eroded = cv2.erode(thresh,kernel)
+eroded = cv2.dilate(eroded,kernel)
+
+answers = cv2.connectedComponentsWithStats(eroded, 4, cv2.CV_32S)
+answers = answers[2]
+answers = answers[1:23]
+j = 0
+Question_no = "1.1","1.2","1.3","1.4","1.5","2.1","2.2","2.3","2.4","2.5","2.6","3.1","3.2","3.3","4.1","4.2","4.3",\
+              "5.1","5.2"
+for i in answers:
+    if i[1] == answers[0][1]:
+        if (i[0] >= 353) & (i[0] <= 391):
+            file.write("Gender: Male \r\n")
+        elif (i[0] >= 392) & (i[0] <= 446):
+            file.write("Gender: Female \r\n")
+    if i[1] == answers[1][1]:
+        # Continue semester values
+        if (i[0] >= 145) & (i[0] <= 185):
+            file.write("Semester: Fall \r\n")
+        if (i[0] >= 190) & (i[0] <= 260):
+            file.write("Semester: Spring \r\n")
+        if (i[0] >= 290) & (i[0] <= 350):
+            file.write("Semester: Summer \r\n")
+    if i[1] == answers[2][1]:
+        # Program value
+        if (i[0] >= 120) & (i[0] <= 150) & ~(i[1] >= 140):
+            file.write("Program: MCTA \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 159) & (i[0] <= 189) & ~(i[1] >= 140):
+            file.write("Program: ENVER \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 198) & (i[0] <= 228) & ~(i[1] >= 140):
+            file.write("Program: BLDG \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 237) & (i[0] <= 277) & ~(i[1] >= 140):
+            file.write("Program: CESS \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 275) & (i[0] <= 305) & ~(i[1] >= 140):
+            file.write("Program: ERGY \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 307) & (i[0] <= 337) & ~(i[1] >= 140):
+            file.write("Program: COMM \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 354) & (i[0] <= 384) & ~(i[1] >= 140):
+            file.write("Program: MANF \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 120) & (i[0] <= 150) & (i[1] >= 140):
+            file.write("Program: LAAR \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 159) & (i[0] <= 189) & (i[1] >= 140):
+            file.write("Program: MATL \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 198) & (i[0] <= 228) & (i[1] >= 140):
+            file.write("Program: CISE \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+        if (i[0] >= 237) & (i[0] <= 277) & (i[1] >= 140):
+            file.write("Program: HAUD \r\n====================================================================== \r\n"
+                       "====================================================================== \r\n")  # done
+
+    while (j != 19) & (i[1] >= answers[3][1]):
+        if i[1] == answers[j+3][1]:
+            # Questions
+            if (i[0] >= 310) & (i[0] <= 340):
+                file.write("Question"+Question_no[j] + " : Strongly Agree \r\n====================================================================== \r\n")
+                j += 1
+                break
+            if (i[0] >= 341) & (i[0] <= 371):
+                file.write("Question"+Question_no[j] + " : Agree \r\n====================================================================== \r\n")
+                j += 1
+                break
+            if (i[0] >= 372) & (i[0] <= 402):
+                file.write("Question"+Question_no[j] + " : Neutral \r\n====================================================================== \r\n")
+                j += 1
+                break
+            if (i[0] >= 403) & (i[0] <= 433):
+                file.write("Question"+Question_no[j] + " : Disagree \r\n====================================================================== \r\n")
+                j += 1
+                break
+            if (i[0] >= 434) & (i[0] <= 474):
+                file.write("Question"+Question_no[j] + " : Strongly Disagree \r\n====================================================================== \r\n")
+                j += 1
+                break
+file.close()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
